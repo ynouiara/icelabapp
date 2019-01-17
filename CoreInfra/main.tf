220472a264a6d94992b32469b7f7ae7d87ac44c1
Youssef Nouiara <youssef.nouiara@d2si.io>
2019-01-17 16:36:57 +0100

    Create a Route Table

--- CoreInfra/main.tf
+++ CoreInfra/main.tf
@@ -24,3 +24,16 @@ resource "aws_internet_gateway" "myig" {
     Name = "icelab"
   }
 }
+
+resource "aws_route_table" "myrt" {
+  vpc_id = "${aws_vpc.myvpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.myig.id}"
+  }
+
+  tags {
+    Name = "icelab"
+  }
+}
